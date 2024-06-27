4bbea7c9c410118c7b74a9f2bdeebbee19c881d2
Vidushi Chaudhary <118211621+Vidushi2456@users.noreply.github.com>
2024-06-27 16:58:16 +0530

    Update UserList.js

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './UserList.css';
+import { Table } from 'antd';
+import 'antd/dist/antd.css';
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
@@ -11,6 +13,22 @@ const UserList = () => {
       .catch(error => console.error('Error fetching users:', error));
   }, []);
 
+const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+  ];
+  return <Table dataSource={users} columns={columns} rowKey="id" />;
+};
+
+  
   return (
     <div className="user-list">
       <table>
